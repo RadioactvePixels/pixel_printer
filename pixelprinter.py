236e99597475d5fc1f58189c8f8a9e63069d82dd
RadioactvePixels <42788312+RadioactvePixels@users.noreply.github.com>
2022-08-18 16:25:46 -0400

    Create pixelprinter.py

---
+++ pixelprinter.py
@@ -0,0 +1,61 @@
+from PIL import Image
+im = Image.open("charmander.png")
+#im = Image.open("pikachu.png")
+pix = im.load()
+rgb_im = im.convert('RGB')
+
+output = "charmander = ["
+for x in range(20):
+	output = output + "["
+	for y in range(20):
+		r, g, b = rgb_im.getpixel((x, y))
+
+		if r == 0 and g == 0 and b == 0:
+			output = output + "b"
+		elif r == 255 and g == 156 and b == 6:
+			output = output + "o"
+		elif r == 255 and g == 255 and b == 255:
+			output = output + "w"
+		elif r == 255 and g == 228 and b == 69:
+			output = output + "y"
+		elif r == 250 and g == 64 and b == 51:
+			output = output + "r"
+		else:
+			output = output + "green"
+		if y != 19:
+			output = output + ","
+
+	if x != 19:
+		output = output + "], "
+	else:
+		output = output + "]]"
+
+# output = "pikachu = ["
+# for x in range(40):
+# 	output = output + "["
+# 	for y in range(40):
+# 		r, g, b = rgb_im.getpixel((x, y))
+# 		if r == 0 and g == 0 and b == 0:
+# 			output += "b"
+# 		elif r == 252 and g == 233 and b == 78:
+# 			output += "y"
+# 		elif r == 242 and g == 163 and b == 58:
+# 			output += "o" #accent color
+# 		elif r == 126 and g == 80 and b == 25:
+# 			output += "br" #brown
+# 		elif r == 235 and g == 50 and b == 35:
+# 			output += "r"
+# 		elif r == 255 and g == 255 and b == 255:
+# 			output += "w"
+# 		else:
+# 			output += "g"
+# 		if y != 39:
+# 			output = output + ","
+
+# 	if x != 39:
+# 		output = output + "], "
+# 	else:
+# 		output = output + "]]"
+
+print(output)
+
